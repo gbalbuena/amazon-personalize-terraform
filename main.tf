b4ddf9c70abbc5a891c4a1fdc231be5bd33d633a
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2020-06-16 14:37:32 +1000

    Add Storage and build

--- main.tf
+++ main.tf
@@ -3,3 +3,25 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "storage" {
+  bucket = "${var.app_name}-storage"
+  acl    = "private"
+}
+
+data "archive_file" "code" {
+  type        = "zip"
+  source_dir  = "${path.root}/lambdas"
+  output_path = "${path.root}/dist/build.zip"
+}
+
+resource "aws_s3_bucket_object" "artefact_s3_object" {
+  bucket = aws_s3_bucket.storage.id
+  key    = "build.zip"
+  source = "dist/build.zip"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("dist/build.zip")
+}
